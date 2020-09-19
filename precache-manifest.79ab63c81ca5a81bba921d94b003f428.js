1065a422f42ecf3f7d7cdffcc05f0f005320e2be
mochenel <freddy980404@gmail.com>
2020-09-19 21:01:19 +0200

    Updates

---
+++ precache-manifest.79ab63c81ca5a81bba921d94b003f428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "242f681f0528bd0c98d700610e7f423a",
+    "url": "/dice/index.html"
+  },
+  {
+    "revision": "6ac19f62fd8109f27efb",
+    "url": "/dice/static/css/main.00327497.chunk.css"
+  },
+  {
+    "revision": "7d7ed07d1c32a4f1c277",
+    "url": "/dice/static/js/2.c4ca40fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dice/static/js/2.c4ca40fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ac19f62fd8109f27efb",
+    "url": "/dice/static/js/main.2efe0749.chunk.js"
+  },
+  {
+    "revision": "6f349f4a9e93576960a4",
+    "url": "/dice/static/js/runtime-main.6581301c.js"
+  }
+]);
